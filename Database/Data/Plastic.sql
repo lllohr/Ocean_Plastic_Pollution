03518f806e0b08c826a36b1ee3de736821901861
Amy Tisland <tislandma@att.net>
2022-07-12 18:09:31 -0700

    Updated & Cleaned MLW_dataz-clean

--- Database/Data/Plastic.sql
+++ Database/Data/Plastic.sql
@@ -1,14 +1,24 @@
---Create new table for country based on plastics
-SELECT md.BeachCountry,
-       md.BeachName,
-	   md.BeachRegionalSea,
-	   md.BeachLength_m,
-	   md.G1 - md.G124
-	   mm.generalcode
-	   mm.category
-INTO plastic_waste
-FROM MLW_data as md
-LEFT JOIN Mlw_meta as mm
-ON (mm.generalcode between md.G1 AND md.G124 )
-WHERE mm.category = "Plastics";
-	  
+
+--Create new table from mlw_meta with plastic only
+SELECT generalcode,
+	   generalname
+INTO plastic_names	   
+FROM Mlw_meta
+WHERE category = 'Plastic';
+SELECT * FROM  plastic_names;
+
+--Create new table for plasticwaste in 2019
+SELECT plastic_waste_mismanagement.Country,
+INTO plastic_waste_countries
+FROM Plastic_waste_mismanagement 
+Where Year ='2019';
+
+-- Create new table from mlw_data with codes G1-G100
+
+SELECT BeachCountry, Beachname, BeachRegionalSea,BeachLength_m,BeachType
+INTO plastic_numbers
+FROM mlw_data
+WHERE COLUMNS = G1 BETWEEN G100;
+
+
+
